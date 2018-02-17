14abcf6232d3a9b87f70b57ceac4cb5f79c480ae
Jennifer Ai <jenniferai123@gmail.com>
2018-02-17 14:12:12 -0500

    button triggers player1 attack animation

--- Hero's Arena/Assets/Scripts/Player1AttackButton.cs
+++ Hero's Arena/Assets/Scripts/Player1AttackButton.cs
@@ -5,12 +5,30 @@ using UnityEngine.UI;
 
 public class Player1AttackButton : MonoBehaviour {
 
+	public GameObject player1; 
+	private Animator anim;
+
+	private void Start() {
+		GetComponent<Button> ().onClick.AddListener (TriggerPlayer1);
+		anim = player1.GetComponent<Animator> ();
+	}
+
+	private void TriggerPlayer1() {
+		anim.SetTrigger ("playerChop");
+
+	}
+}
+	
+/*
+	public GameObject player;
+	public Button Player1Attack;
 	private Animator anim;
 
 	// Use this for initialization
 	void Start () {
-		anim = GetComponent<Animator> ();
-		
+		anim = player.GetComponent<Animator> ();
+		Button btn = Player1Attack.GetComponent<Button> ();
+		btn.onClick.AddListener (Attack);
 	}
 	
 	// Update is called once per frame
@@ -18,7 +36,12 @@ public class Player1AttackButton : MonoBehaviour {
 		
 	}
 
-	public void Button_Onclick() {
+	void Attack() {
+		anim.SetTrigger ("playerChop");
+	}
+
+	public void Button_Click() {
 		anim.SetTrigger ("playerChop");
 	}
 }
+*/
