d7d5ba234ca304645e27ed369558ba11a6bbb5d4
Jennifer Ai <jenniferai123@gmail.com>
2018-03-30 17:39:24 -0400

    added checkpoint2 and modified mainscene for ui
    currently working on info button

---
+++ Hero's Arena/Assets/Scripts/PopupWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PopupWindow : MonoBehaviour {
+
+	public GameObject window;
+	public Text characterInfo;
+
+	public void Show (string message) {
+		characterInfo.text = message;
+		window.SetActive (true);
+	}
+
+	public void Hide() {
+		window.SetActive (false);
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
